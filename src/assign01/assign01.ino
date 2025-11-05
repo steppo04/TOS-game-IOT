86ed22136aa772e3a64de0d5f01aaa5db5fe94ac
Giuseppe Fusco <giuseppe.fusco9@studio.unibo.it>
2025-11-05 09:55:53 +0100

    implements sequenceGen and first implementation of the class .ino

--- src/assign01/assign01.ino
+++ src/assign01/assign01.ino
@@ -1,10 +1,57 @@
+#include "config.h"
+#include "input.h"
 #include "output.h"
+#include "game.h"
+
+
+GameState currentState = STATE_IDLE;
+unsigned long lastActionTime = 0;
+
 void setup() {
+  initHardware();
   lcdInit();
   showWelcomeMessage();
-
 }
+
 
 void loop() {
+  readInputs();
 
+  switch (currentState) {
+    case STATE_IDLE:
+      pulseLedS();
+      if (buttonPressed(BTN_B1)) {
+        currentState = STATE_START;
+        lastActionTime = millis();
+      } else if (millis() - lastActionTime > 10000) {
+        enterDeepSleep();
+      }
+      break;
+
+    case STATE_START:
+      startGame();
+      currentState = STATE_PLAYING;
+      break;
+
+    case STATE_PLAYING:
+      if (playRound()) {
+        currentState = STATE_ROUND_OK;
+      } else if (isGameOver()) {
+        currentState = STATE_GAME_OVER;
+      }
+      break;
+
+    case STATE_ROUND_OK:
+      showGoodMessage();
+      delay(1000);
+      currentState = STATE_PLAYING;
+      break;
+
+    case STATE_GAME_OVER:
+      handleGameOver();
+      delay(10000);
+      currentState = STATE_IDLE;
+      break;
+  }
 }
+
--- src/assign01/game.cpp
+++ src/assign01/game.cpp
@@ -3,6 +3,7 @@
 #include "input.h"
 
 int playerIndex;
+int F;
 int score;
 unsigned long timeLimit;
 unsigned long roundStartTime;
@@ -12,26 +13,43 @@ void startGame() {
   playerIndex = 0;
   score = 0;
   gameOverFlag = false;
+  timeLimit = BASE_T1;
   setTimeLimitDifficulty();
-
+  lcd.clear();
+  lcd.show("Go!");
+  delay(1000);
 }
 
 void setTimeLimitDifficulty() {
-  timeLimit = BASE_T1;
   switch(readDifficulty()) {
     case 1:
+      F = 0.9;
       break;
     case 2:
-      timeLimit -= 1000;
+      F = 0.7;
       break;
     case 3:
-      timeLimit -= 2000;
+      F = 0.5;
       break;
     case 4:
-      timeLimit -= 3000;
+      F = 0.3;
+      break;
+      
   }
 }
 
+void generateSequence() {
+  int used[5] = {0};
+  for (int i = 0; i < 4; i++) {
+    int num;
+    do {
+      num = random(1, 5);
+    } while(used[num]);
+    used[num] = 1;
+    sequence[i] = num;
+  }
+
+}
 
 bool playRound() {
 
@@ -42,9 +60,5 @@ bool isGameOver() {
 }
 
 void handleGameOver() {
-
-}
-
-void generateSequence() {
 
 }
